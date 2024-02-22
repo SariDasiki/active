c96c2eeb1784b688a7c1538f4295fda4a57615b9
SariDasiki <stu14e21@gmail.com>
2024-02-22 15:57:54 +0900

    second commit

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,27 +1,34 @@
 class ExercisesController < ApplicationController
   def exercise1
-    # 【要件】注文されていないすべての料理を返すこと
+    # 注文されていないすべての料理
     #   * left_outer_joinsを使うこと
-    @foods = Food
+    @foods = Food.left_outer_joins(:order_foods)
+    .where(order_foods: { id: nil })
   end
 
   def exercise2
-    # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
+    # 【要件】注文されていない料理を提供してる店 
     #   * left_outer_joinsを使うこと
-    @shops = Shop
+    # @shops = Shop.left_outer_joins(:)
+    # .select(:name)
+    # .from(foods, order_foods, shops, )
+    # .order(foods: :name)
+    # .where(id:nill)
   end
 
   def exercise3 
-    # 【要件】配達先の一番多い住所を返すこと
-    #   * joinsを使うこと
-    #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address
+    # 【要件】配達先の一番多い住所を返すこと maxとcount
+    #   * 取得したAddressのインスタンスにorders_countと
+    # 呼びかけると注文の数を返すこと
+    # @address = Address.joins(:movies).count
+    # @address = orders_count
   end
 
   def exercise4 
-    # 【要件】一番お金を使っている顧客を返すこと
+    # 一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer
+    # @customer.joins.(:).order(foods{: price: :asc))
+    # @customer = Customer.foods_price_sum
   end
 end
